559f27d52c5e74de5a37acc142de1e1e0cb25309
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2023-06-29 22:48:29 +0300

    Retrive books from json

--- GraphApi/Query.cs
+++ GraphApi/Query.cs
@@ -1,19 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 using System.Linq;
 using WishList.Entities;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace WishList.GraphApi;
 public class Query
 {
+    private readonly IConfiguration configuration;
+
+    public Query(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
     public Book GetBook(string id)
     {
+        var pathToBooks = configuration.GetValue<string>("DataPath:LocalBookListData");
         /* Retrieve book by id */
-        var books = new List<Book>
-        {
-            new Book { Id = "1", Title = "Book 1", AuthorId = "1" },
-            new Book { Id = "2", Title = "Book 2", AuthorId = "2" },
-            new Book { Id = "3", Title = "Book 3", AuthorId = "1" }
-        };
+        //var books = new List<Book>
+        //{
+        //    new Book { Id = "1", Title = "Book 1", AuthorId = "1" },
+        //    new Book { Id = "2", Title = "Book 2", AuthorId = "2" },
+        //    new Book { Id = "3", Title = "Book 3", AuthorId = "1" }
+        //};
+
+        //return books.FirstOrDefault(book => book.Id == id);
+
+        string json = File.ReadAllText(pathToBooks);
+        List<Book> books = JsonConvert.DeserializeObject<List<Book>>(json);
 
         return books.FirstOrDefault(book => book.Id == id);
     }
