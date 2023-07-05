3d90abf0c8b5d54b09a79d5a5327e00f3a34c0a1
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2023-07-05 19:46:43 +0300

    Add navigation items

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -26,6 +26,12 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
                                 <NavItem>
                                     <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                                 </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/books">Books</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/book-details">Book Details</NavLink>
+                                </NavItem>
                             </ul>
                         </Collapse>
                     </Container>
