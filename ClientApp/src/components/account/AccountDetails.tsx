f1c41552c81e776a0ef9efaa7d2270d63eae4a8b
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2023-07-11 23:33:54 +0300

    Remove logout from account details

--- ClientApp/src/components/account/AccountDetails.tsx
+++ ClientApp/src/components/account/AccountDetails.tsx
@@ -2,21 +2,18 @@ type Props = {
   name: string;
   email: string;
   pictureSrc?: string;
-  handleSignOut: () => void;
 }
 
 const AccountDetails = ({
   name,
   email,
   pictureSrc,
-  handleSignOut,
 }: Props) => {
   return (
     <div>
       <img src={pictureSrc}></img>
       <h3>{name}</h3>
       <h3>{email}</h3>
-      <button onClick={handleSignOut}>SignOut</button>
     </div>
   )
 }
